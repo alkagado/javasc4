36fdca0704e41ffea4ba8bf613555b279dd2de4c
alkagado <38248590+alkagado@users.noreply.github.com>
2018-04-16 21:50:56 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var a = 3;
+var b = 8;
+var value = (a * a) - (2 * a * b) - (b * b);
+console.log(value);
+
+
+if (value > 0) { console.log('wynik dodatni'); }
+
+else if (value < 0) { console.log('wynik ujemny'); }
